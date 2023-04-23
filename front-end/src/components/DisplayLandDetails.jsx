afe9377ca1ad55a082d94baf4ac0b5d3755de9c7
keshav pareek <kkeshavpareekk@gmail.com>
2023-04-23 10:52:51 +0530

    final changes done

--- front-end/src/components/DisplayLandDetails.jsx
+++ front-end/src/components/DisplayLandDetails.jsx
@@ -1,35 +1,75 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
 const DisplayLandDetails = (props) => {
+  const [Long, setLong] = useState(0);
+  const [Lang, setLang] = useState(0);
+
+  useEffect(() => {
+    let longString = props?.longitude.toString();
+    let latString = props?.latitude.toString();
+    let longNum = longString.slice(0, 2) + "." + longString.slice(2);
+    let latNum = latString.slice(0, 2) + "." + latString.slice(2);
+    setLong(parseFloat(longNum));
+    setLang(parseFloat(latNum));
+  }, [props]);
+
+  console.log(Long, Lang);
   return (
     <>
-    {
-          <div className='explore-result'>
-            <div className='row'>
-              <div className='col-12 col-md-6'>
-                <p><b>Owner:</b> {props.owner}</p>
-                <p><b>Survey Number:</b> {props.surveyNo}</p>
-                <p><b>Property ID:</b> {props.propertyId}</p>
-                <p><b>Market Value:</b> {props.marketValue}</p>
-              </div>
-
-              <div className='col-12 col-md-6'>
-                <p><b>State:</b> {props.state}</p>
-                <p><b>District:</b> {props.district}</p>
-                <p><b>City:</b> {props.city}</p>
-                <p><b>Size:</b> {props.sqft} sq. ft.</p>
-              </div>
-            </div>
-            {
-            (props.available) ? 
-              <button className='marked-available'><b>Marked Available</b></button>
-              :
-              <button className='mark-available-btn' onClick={() => {props.markAvailable(props.index)}} ><b>Mark Available</b></button>
-            }
-          </div> 
-    }
+      <div className="explore-result">
+        <div className="row">
+          <div className="col-6">
+            <img
+              src={`https://maps.googleapis.com/maps/api/staticmap?center=${Lang},${Long}&zoom=12&size=400x400&key=AIzaSyDyqBo5lmmRiFu1dpBn_r_atvoE3qwO3lM`}
+            />
+          </div>
+          <div className="col-12 col-md-6">
+            <p>
+              <b>Owner:</b> {props.owner}
+            </p>
+            <p>
+              <b>Survey Number:</b> {props.surveyNo}
+            </p>
+            <p>
+              <b>Market Value:</b> {props.marketValue}
+            </p>
+            <p>
+              <b>State:</b> {props.state}
+            </p>
+            <p>
+              <b>District:</b> {props.district}
+            </p>
+            <p>
+              <b>City:</b> {props.city}
+            </p>
+            <p>
+              <b>Size:</b> {props.sqft} sq. ft.
+            </p>
+            <p>
+              <b>latitude:</b> {props.latitude}
+            </p>
+            <p>
+              <b>longitude:</b> {props.longitude}
+            </p>
+          </div>
+        </div>
+        {props.available ? (
+          <button className="marked-available">
+            <b>Marked Available</b>
+          </button>
+        ) : (
+          <button
+            className="mark-available-btn"
+            onClick={() => {
+              props.markAvailable(props.index);
+            }}
+          >
+            <b>Mark Available</b>
+          </button>
+        )}
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default DisplayLandDetails
+export default DisplayLandDetails;
--- front-end/src/components/RegisterLand.jsx
+++ front-end/src/components/RegisterLand.jsx
@@ -1,89 +1,198 @@
-import React, { useState } from 'react'
-import '../css/RegisterLand.css'
+import React, { useState } from "react";
+import "../css/RegisterLand.css";
 
 const RegisterLand = (props) => {
-
   const { provider, web3, contract } = props.myWeb3Api;
   const account = props.account;
 
   const [landDetails, setLandDetials] = useState({
-    state:"", district:"", city:"", propertyId:"", surveyNo:"", owner:"", marketValue:"", size:""
-  }) 
+    state: "",
+    district: "",
+    city: "",
+    propertyId: "",
+    surveyNo: "",
+    owner: "",
+    marketValue: "",
+    size: "",
+    longitude: "",
+    latitude: "",
+  });
 
-  const onChangeFunc = (event) =>{
-    const {name, value} = event.target;
-    setLandDetials({...landDetails, [name]:value});
-  }
+  const onChangeFunc = (event) => {
+    const { name, value } = event.target;
+    setLandDetials({ ...landDetails, [name]: value });
+  };
 
-  const handleOnClick = async () =>{
-    await contract.registerLand(landDetails.state, landDetails.district, landDetails.city, landDetails.propertyId, landDetails.surveyNo, landDetails.owner, landDetails.marketValue, landDetails.size, {
-      from: account
-    })
-    console.log(landDetails)
-    setLandDetials({state:"", district:"", city:"", propertyId:"", surveyNo:"", owner:"", marketValue:"", size:""})
-  }
-
-
+  const handleOnClick = async () => {
+    await contract.registerLand(
+      landDetails.state,
+      landDetails.district,
+      landDetails.city,
+      landDetails.propertyId,
+      landDetails.surveyNo,
+      landDetails.owner,
+      landDetails.marketValue,
+      landDetails.size,
+      landDetails.latitude,
+      landDetails.longitude,
+      {
+        from: account,
+      }
+    );
+    console.log(landDetails);
+    setLandDetials({
+      state: "",
+      district: "",
+      city: "",
+      propertyId: "",
+      surveyNo: "",
+      owner: "",
+      marketValue: "",
+      size: "",
+      longitude: "",
+      latitude: "",
+    });
+  };
 
   return (
-    <div className='container registerLand-maindiv'>
-      <div className='row'>
-
-         {/* left form */}
-        <div className='col-12 col-sm-6'>
-            <form method='POST' className='admin-form'>
-                <div className='form-group'>
-                    <label>State</label>
-                    <input type="text" className="form-control" name="state" placeholder="Enter State" 
-                    autoComplete="off" value={landDetails.state} onChange={onChangeFunc}/>
-                </div>
-                <div className='form-group'>
-                    <label>District</label>
-                    <input type="text" className="form-control" name="district" placeholder="Enter district" 
-                    autoComplete="off" value={landDetails.district} onChange={onChangeFunc}/>
-                </div>
-                <div className='form-group'>
-                    <label>City</label>
-                    <input type="text" className="form-control" name="city" placeholder="Enter city" 
-                    autoComplete="off" value={landDetails.city} onChange={onChangeFunc}/>
-                </div>
-                <div className='form-group'>
-                    <label>Property ID</label>
-                    <input type="number" className="form-control" name="propertyId" placeholder="Enter property ID" 
-                    autoComplete="off" value={landDetails.propertyId} onChange={onChangeFunc}/>
-                </div>
-            </form>
+    <div className="container registerLand-maindiv">
+      <div className="row">
+        <div className="col-12 col-sm-6">
+          <form method="POST" className="admin-form">
+            <div className="form-group">
+              <label>State</label>
+              <input
+                type="text"
+                className="form-control"
+                name="state"
+                placeholder="Enter State"
+                autoComplete="off"
+                value={landDetails.state}
+                onChange={onChangeFunc}
+              />
+            </div>
+            <div className="form-group">
+              <label>District</label>
+              <input
+                type="text"
+                className="form-control"
+                name="district"
+                placeholder="Enter district"
+                autoComplete="off"
+                value={landDetails.district}
+                onChange={onChangeFunc}
+              />
+            </div>
+            <div className="form-group">
+              <label>City</label>
+              <input
+                type="text"
+                className="form-control"
+                name="city"
+                placeholder="Enter city"
+                autoComplete="off"
+                value={landDetails.city}
+                onChange={onChangeFunc}
+              />
+            </div>
+            <div className="form-group">
+              <label>Property ID</label>
+              <input
+                type="number"
+                className="form-control"
+                name="propertyId"
+                placeholder="Enter property ID"
+                autoComplete="off"
+                value={landDetails.propertyId}
+                onChange={onChangeFunc}
+              />
+            </div>
+            <div className="form-group">
+              <label>Latitude</label>
+              <input
+                type="number"
+                className="form-control"
+                name="latitude"
+                placeholder="Enter latitude"
+                autoComplete="off"
+                value={landDetails.latitude}
+                onChange={onChangeFunc}
+              />
+            </div>
+          </form>
         </div>
 
         {/* right form */}
-        <div className='col-12 col-sm-6'>
-          <form method='POST' className='admin-form'>
-            <div className='form-group'>
-                <label>Survey Number</label>
-                <input type="number" className="form-control" name="surveyNo" placeholder="Enter survey number" 
-                autoComplete="off" value={landDetails.surveyNo} onChange={onChangeFunc}/>
+        <div className="col-12 col-sm-6">
+          <form method="POST" className="admin-form">
+            <div className="form-group">
+              <label>Survey Number</label>
+              <input
+                type="number"
+                className="form-control"
+                name="surveyNo"
+                placeholder="Enter survey number"
+                autoComplete="off"
+                value={landDetails.surveyNo}
+                onChange={onChangeFunc}
+              />
             </div>
-            <div className='form-group'>
-                <label>Owner Address</label>
-                <input type="text" className="form-control" name="owner" placeholder="Enter owner address" 
-                autoComplete="off" value={landDetails.owner} onChange={onChangeFunc}/>
+            <div className="form-group">
+              <label>Owner Address</label>
+              <input
+                type="text"
+                className="form-control"
+                name="owner"
+                placeholder="Enter owner address"
+                autoComplete="off"
+                value={landDetails.owner}
+                onChange={onChangeFunc}
+              />
             </div>
-            <div className='form-group'>
-                <label>Market Value</label>
-                <input type="number" className="form-control" name="marketValue" placeholder="Enter market value" 
-                autoComplete="off" value={landDetails.marketValue} onChange={onChangeFunc}/>
+            <div className="form-group">
+              <label>Market Value</label>
+              <input
+                type="number"
+                className="form-control"
+                name="marketValue"
+                placeholder="Enter market value"
+                autoComplete="off"
+                value={landDetails.marketValue}
+                onChange={onChangeFunc}
+              />
             </div>
-            <div className='form-group'>
-                <label>Size</label>
-                <input type="number" className="form-control" name="size" placeholder="Enter size (sq. ft.)" 
-                autoComplete="off" value={landDetails.size} onChange={onChangeFunc}/>
+            <div className="form-group">
+              <label>Size</label>
+              <input
+                type="number"
+                className="form-control"
+                name="size"
+                placeholder="Enter size (sq. ft.)"
+                autoComplete="off"
+                value={landDetails.size}
+                onChange={onChangeFunc}
+              />
+            </div>
+            <div className="form-group">
+              <label>Longitude</label>
+              <input
+                type="number"
+                className="form-control"
+                name="longitude"
+                placeholder="Enter longitude"
+                autoComplete="off"
+                value={landDetails.longitude}
+                onChange={onChangeFunc}
+              />
             </div>
           </form>
         </div>
       </div>
-      <button className='admin-form-btn' onClick={handleOnClick}>Submit</button>
+      <button className="admin-form-btn" onClick={handleOnClick}>
+        Submit
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default RegisterLand
+export default RegisterLand;
